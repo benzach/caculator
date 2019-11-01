a2a6baa876d1bc70514cfe038473a7f6da07a1ae
benzach <ephamdatllc@gmail.com>
2019-11-01 03:20:36 -0500

    Stretch goal 2. ctrl + c exit program

--- calculator/Program.cs
+++ calculator/Program.cs
@@ -7,6 +7,7 @@ namespace calculator
 {
     class Program
     {
+        private static bool keepRunning = true;
         static void Main(string[] args)
         {
             var serviceProvider = new ServiceCollection()
@@ -16,19 +17,21 @@ namespace calculator
                                       .BuildServiceProvider();
             var CalculatorService = serviceProvider.GetService<ICalculatorService>();
             Console.WriteLine("Enter x to exit program");
-            while(true)
+            while(keepRunning)
             {
                 
                 Console.WriteLine("Please enter 2 operands separated by comma to add:");
+                Console.CancelKeyPress += Console_CancelKeyPress;
+
+
                 var input = Console.ReadLine();
-                if(input.ToLower()=="x")
+                if(input==null || input.ToLower()=="x")
                 {
                     break;
                 }
                 try
                 {
                     var sumResult = CalculatorService.AddOperands(input);
-                    //Console.WriteLine($"sum of {input} = {sum}");
                     Console.WriteLine($"{string.Join('+', sumResult.operands)} = {sumResult.sum}");
                 }catch(Exception ex)
                 {
@@ -37,6 +40,12 @@ namespace calculator
 
             }
             
+        }
+
+        private static void Console_CancelKeyPress(object sender, ConsoleCancelEventArgs e)
+        {
+            e.Cancel = true;
+            Program.keepRunning = false;
         }
     }
 }
